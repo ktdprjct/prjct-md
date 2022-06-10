ad8d4b4a69ef94a8a7355b84923548e7127ce7b4
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2022-06-10 22:00:05 +0800

    update _expired.js

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -7,7 +7,7 @@ export async function all(m) {
     if (!chats.expired)
         return !0
     if (+new Date() > chats.expired) {
-        let text = `${monospace("====> I N F O <====")}\n\nSudah waktunya *ktdprjct メ bot* untuk meninggalkan grup, jika ingin *Bot* stay di grup ini, silahkan sewa bot ke *Owner* wa.me/62895342581896`
+        let text = `${monospace("====> P E N G U M U A N <====")}\n\nSudah waktunya *ktdprjct メ bot* untuk meninggalkan grup, jika ingin *Bot* stay di grup ini, silahkan sewa bot ke *Owner* wa.me/62895342581896`
         await this.sendHydrated(m.chat, text, author, fs.readFileSync('./src/logo.jpg'), waowner,`My Owner`, null, null, [[null, 'bye 👋'], [null, null]], null, { asLocation: true })
         await this.groupLeave(m.chat)
         chats.expired = null
