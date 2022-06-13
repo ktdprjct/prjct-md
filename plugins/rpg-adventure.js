78a961a093c4c0ae6f556374deb0ccce66990704
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2022-06-13 10:05:19 +0800

    update rpg-adventure.js

--- plugins/rpg-adventure.js
+++ plugins/rpg-adventure.js
@@ -1,78 +1,90 @@
-const cooldown = 300000
 let handler = async (m, { conn, usedPrefix }) => {
+  try {
     let user = global.db.data.users[m.sender]
-    let timers = (cooldown - (new Date - user.lastadventure))
-    if (user.health < 80) return m.reply(`
-Requires at least 80 ❤️Healths for the adventure!!
-please buy ❤️Healths first by typing *${usedPrefix}buy potion <quantity>*,
-and type *${usedPrefix}heal <quantity>* to use potions
-`.trim())
-    if (new Date - user.lastadventure <= cooldown) return m.reply(`
-You're already adventure!!, please wait *🕐${timers.toTimeString()}*
-`.trim())
-    const rewards = reward(user)
-    let text = 'you\'ve been adventure and lost'
-    for (const lost in rewards.lost) if (user[lost]) {
-        const total = rewards.lost[lost].getRandom()
-        user[lost] -= total * 1
-        if (total) text += `\n*${global.rpg.emoticon(lost)}${lost}:* ${total}`
-    }
-    text += '\n\nBut you got'
-    for (const rewardItem in rewards.reward) if (rewardItem in user) {
-        const total = rewards.reward[rewardItem].getRandom()
-        user[rewardItem] += total * 1
-        if (total) text += `\n*${global.rpg.emoticon(rewardItem)}${rewardItem}:* ${total}`
-    }
-    conn.sendHydrated(m.chat, text.trim(), author, 'https://telegra.ph/file/0ad816a09fa00dd2f134c.jpg', linkgc, textnya, null, null, [['Inventory', '.Inventory']], null, { asLocation: true })                                 
-    user.lastadventure = new Date * 1
-}
-handler.help = ['adventure', 'petualang', 'berpetualang', 'mulung']
-handler.tags = ['rpg']
-handler.command = /^(adventure|(ber)?petualang(ang)?|mulung)$/i
+    let __timers = (new Date - user.lastadventure)
+    let _timers = (3600000 - __timers)
+    let timers = clockString(_timers)
+    if (global.db.data.users[m.sender].health > 79) {
+      if (new Date - global.db.data.users[m.sender].lastadventure > 3600000) {                         
+        let armor = user.armor
+        let rubah = user.rubah
+        let kuda = user.kuda
+        let kucing = user.kucing
+        let serigala = user.serigala
+        let _health = `${Math.floor(Math.random() * 101)}`.trim()
+        let health = (_health * 1)
+        let exp = `${Math.floor(Math.random() * 10000)}`.trim()
+        let money = `${Math.floor(Math.random() * 100000)}`.trim()
+        let _potion = ['1','2','3']
+        let potion = pickRandom(_potion)
+        let _sampah = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50']
+        let sampah = pickRandom(_sampah)
+        let _diamond = ['1','2','3','4','5','6','7','8','9','10']
+        let diamond = pickRandom(_diamond)
+        let _common = ['1','2','3']
+        let common = pickRandom(_common)
+        let _uncommon = ['1','2','1','2']
+        let uncommon = pickRandom(_uncommon)
+        let _mythic = pickRandom(['1','3','1','1','2'])
+        let mythic = (_mythic * 1)
+        let _legendary = pickRandom(['1','3','1','1','2'])
+        let legendary = (_legendary * 1)
+        let itemrand = [`*Selamat anda mendapatkan item rare yaitu*\n${mythic} 🗳️ Mythic Crate`,`*Selamat kamu mendapatkan item rare yaitu*\n${legendary} 🗃️ Legendary Crate`]
+        let rendem = `${pickRandom(itemrand)}`
+        let name = await conn.getName(m.sender)
+        let str = `
+*${name}* Kamu telah berpetualang, dan sekarang nyawamu berkurang ❤️ -${health * 1} karena Kamu telah berpetualang sampai ${pickRandom(['Jepang', 'Korea', 'Bali', 'Amerika', 'Iraq', 'Arab', 'Pakistan', 'German', 'Finlandia', 'Ke bawa dunia mimpi', 'Ujung dunia', 'Mars', 'Bulan', 'Pluto', 'Matahari', 'Hatinya dia', '...'])} dan mendapatkan
 
-handler.cooldown = cooldown
-handler.disabled = false
+✉️ *exp:* ${exp} 
+💵 *money:* ${money}
+🗑 *sampah:* ${sampah}${potion == 0 ? '' : `\n🥤 *Potion:* ` + potion + ''}${diamond == 0 ? '' : `\n💎 *diamond:* ` + diamond + ''}${common == 0 ? '' : `\n📦 *common:* ` + common + ''}${uncommon == 0 ? '' : `\n🎁 *uncommon:* ` + uncommon + ''}
+`.trim()
+        
+        setTimeout(() => {
+          conn.sendHydrated(m.chat, str, author, 'https://telegra.ph/file/0ad816a09fa00dd2f134c.jpg', linkgc, textnya, null, null, [['Inventory', '.Inventory']], m, { asLocation: true })                                 
+        }, 0)
+        setTimeout(() => {
+           conn.reply(m.chat, rendem, m)
+        }, 3000)
+        
+        user.health -= health * 1
+        user.exp += exp * 1
+        user.tiketcoin += 1
+        user.money += money * 1
+        user.potion += potion * 1
+        user.diamond += diamond * 1
+        user.common += common * 1 
+        user.uncommon += uncommon * 1
+        user.sampah += sampah * 1
+        user.mythic += mythic * 1
+        user.legendary += legendary * 1
+        user.lastadventure = new Date * 1
+      } else conn.reply(m.chat, `Anda sudah berpetualang dan kelelahan, silahkan coba *${timers}* lagi`, m)
+    } else conn.reply(m.chat, 'Minimal 80 health untuk bisa berpetualang, beli nyawa dulu dengan ketik *' + usedPrefix + 'shop buy potion <jumlah>*\ndan ketik *' + usedPrefix + 'use potion <jumlah>*', m)
+  } catch (e) {
+        console.log(e)
+        conn.reply(m.chat, 'Error', m)
+    }
+  
+}
+handler.help = ['adventure', 'petualang', 'berpetualang']
+handler.tags = ['rpg']
+handler.command = /^(adventure|(ber)?petualang(ang)?)$/i
+
+handler.limit = true
+handler.group = true
+handler.fail = null
 
 export default handler
 
-function reward(user = {}) {
-    let rewards = {
-        reward: {
-            money: 201,
-            exp: 301,
-            trash: 101,
-            potion: 2,
-            rock: 2,
-            wood: 2,
-            string: 2,
-            common: 2 * (user.dog && (user.dog > 2 ? 2 : user.dog) * 1.2 || 1),
-            uncommon: [0, 0, 0, 1, 0].concat(
-                new Array(5 - (
-                    (user.dog > 2 && user.dog < 6 && user.dog) || (user.dog > 5 && 5) || 2
-                )).fill(0)
-            ),
-            mythic: [0, 0, 0, 0, 0, 1, 0, 0, 0].concat(
-                new Array(8 - (
-                    (user.dog > 5 && user.dog < 8 && user.dog) || (user.dog > 7 && 8) || 3
-                )).fill(0)
-            ),
-            legendary: [0, 0, 0, 0, 0, 0, 0, 1, 0, 0].concat(
-                new Array(10 - (
-                    (user.dog > 8 && user.dog) || 4
-                )).fill(0)
-            ),
-            iron: [0, 0, 0, 1, 0, 0],
-            gold: [0, 0, 0, 0, 0, 1, 0],
-            diamond: [0, 0, 0, 0, 0, 0, 1, 0].concat(
-                new Array(5 - (
-                    (user.fox < 6 && user.fox) || (user.fox > 5 && 5) || 0
-                )).fill(0)
-            ),
-        },
-        lost: {
-            health: 101 - user.cat * 4,
-            armordurability: (15 - user.armor) * 7
-        }
-    }
-    return rewards
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)]
+}
+
+function clockString(ms) {
+  let h = Math.floor(ms / 3600000)
+  let m = Math.floor(ms / 60000) % 60
+  let s = Math.floor(ms / 1000) % 60
+  console.log({ms,h,m,s})
+  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
 }
